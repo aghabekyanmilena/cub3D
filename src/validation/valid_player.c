3a7dcd160994276d6525af68df13eddbf8544769
Milena Aghabekyan <miaghabe@c2r1s2.42yerevan.am>
2025-07-31 18:04:47 +0400

    ban

--- src/validation/valid_player.c
+++ src/validation/valid_player.c
@@ -1,32 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.c                                            :+:      :+:    :+:   */
+/*   valid_player.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: miaghabe <miaghabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/29 17:30:49 by miaghabe          #+#    #+#             */
-/*   Updated: 2025/07/29 17:31:47 by miaghabe         ###   ########.fr       */
+/*   Created: 2025/07/31 17:22:14 by miaghabe          #+#    #+#             */
+/*   Updated: 2025/07/31 17:28:25 by miaghabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-void	exit_with_error(char *msg)
+int	val_player(char c)
 {
-	printf("Error\n%s\n", msg);
-	exit(EXIT_FAILURE);
+	if (c == 'n' || c == 'N'
+		|| c == 'w' || c == 'W'
+		|| c == 's' || c == 'S'
+		|| c == 'e' || c == 'E')
+		return 1;
+	else
+		return 0;
 }
-
-void	free_split(char **split)
-{
-	int	i;
-
-	i = 0;
-	if (!*split)
-		return ;
-	while (split[i])
-		free(split[i++]);
-	free(split);
-}
-

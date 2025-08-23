9e52a3d5290085a8b8bbda6f0ba196f69c98ecd7
Anush Tserunyan <atseruny@c2r1s4.42yerevan.am>
2025-08-23 15:12:01 +0400

    ay hima okay a

--- src/valid.c
+++ src/valid.c
@@ -6,7 +6,7 @@
 /*   By: atseruny <atseruny@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/02 15:06:44 by miaghabe          #+#    #+#             */
-/*   Updated: 2025/08/23 14:58:50 by atseruny         ###   ########.fr       */
+/*   Updated: 2025/08/23 15:11:08 by atseruny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ int	check_filename(char *filename)
 {
 	char	*name;
 
-	name = ft_strrchr(filename, '.');
-	if (!name || ft_strcmp(name, ".cub") != 0 || (filename + 4))
+	name = ft_strchr(filename, '.');
+	if (!name || ft_strcmp(name, ".cub") != 0 || ft_strlen(name) != 4)
 	{
 		printf("Invalid file name\n");
 		return (0);

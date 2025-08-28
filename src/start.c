770d873563ef717cb313d83490b5165a7b0f7cc5
Anush <tserunyananush@gmail.com>
2025-08-28 12:34:26 +0400

    seg

--- src/start.c
+++ src/start.c
@@ -6,7 +6,7 @@
 /*   By: anush <anush@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 15:29:20 by atseruny          #+#    #+#             */
-/*   Updated: 2025/08/27 17:38:48 by anush            ###   ########.fr       */
+/*   Updated: 2025/08/28 12:32:28 by anush            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,11 +57,11 @@ void	get_player_struct(t_config *config)
 void	put_back(t_data *data)
 {
 	int i=0;
-	int j=0;
-	while (i<1000)
+	int j;
+	while (i<WIDTH)
 	{
 		j=0;
-		while(j<1800)
+		while(j<LENGTH)
 		{
 			if (i>500)
 				mlx_pixel_put(data->mlx, data->win, j, i, 0x000000);
@@ -73,26 +73,49 @@ void	put_back(t_data *data)
 	}
 }
 
+int rgb_to_int(t_color c)
+{
+	return ((c.r & 0xFF) << 16) | ((c.g & 0xFF) << 8) | (c.b & 0xFF);
+}
+
+void	draw_buf(t_data *data, int buffer[WIDTH][LENGTH])
+{
+	int i=0;
+	int j;
+	while (i<LENGTH)
+	{
+		j = 0;
+		while(j<WIDTH)
+		{
+			mlx_pixel_put(data->mlx, data->win, j, i, buffer[j][i]);
+			j++;
+		}
+		i++;
+	}
+}
+
 int	start_ray_casting(t_config *config)
 {
-	int	x;
-	double wallX; //where exactly the wall was hit
-	get_player_struct(config);
-	put_back(&config->data);
+	int		x;
+	int		y;
+	double	wallX; //where exactly the wall was hit
 
-	config->ray.hit = '0';
+	// int buffer[LENGTH][WIDTH];
+	get_player_struct(config);
+	// put_back(&config->data);
 	while (1)
 	{
 		x = 0;
 		while (x < WIDTH)
 		{
-			config->ray.camera_x = 2 * x / WIDTH - 1;
+			config->ray.hit = '0';
+			config->ray.camera_x = 2 * x / (double)WIDTH - 1;
 			config->ray.rayDir_x = config->player.dir_x + config->player.plane_x * config->ray.camera_x;
 			config->ray.rayDir_y = config->player.dir_y + config->player.plane_y * config->ray.camera_x;
-			if (config->ray.rayDir_x == 0 || config->ray.rayDir_x == 0)
-				return (0);
-			config->ray.deltaDist_x = fabs(1 / config->ray.rayDir_x);
-			config->ray.deltaDist_y = fabs(1 / config->ray.rayDir_y);
+
+			config->ray.deltaDist_x = (config->ray.rayDir_x == 0) ? 1e30 : fabs(1 / config->ray.rayDir_x);
+			config->ray.deltaDist_y = (config->ray.rayDir_y == 0) ? 1e30 : fabs(1 / config->ray.rayDir_y);
+
 			if (config->ray.rayDir_x < 0)
 			{
 				config->player.step_x = -1;
@@ -113,8 +136,11 @@ int	start_ray_casting(t_config *config)
 				config->player.step_y = 1;
 				config->ray.sideDist_y = (config->player.map_y + 1.0 - config->player.pos_y) * config->ray.deltaDist_y;
 			}
+			// printf("%c\n", config->ray.hit);
 			while (config->ray.hit == '0')
 			{
+				printf("%c\n", config->map[config->player.map_x][config->player.map_y]);
+
 				if (config->ray.sideDist_x < config->ray.sideDist_y)
 				{
 					config->ray.sideDist_x += config->ray.deltaDist_x;
@@ -127,9 +153,11 @@ int	start_ray_casting(t_config *config)
 					config->player.map_y += config->player.step_y;
 					config->ray.side = 1;
 				}
-				if (config->map[config->player.map_x][config->player.map_y] != '0')
-					config->ray.hit = config->map[config->player.map_x][config->player.map_y];
+				if (config->map[config->player.map_x][config->player.map_y] == '1')
+					config->ray.hit = '1';
+				
 			}
+
 			if (config->ray.side == 0)
 				config->ray.wallDist = (config->ray.sideDist_x - config->ray.deltaDist_x);
 			else
@@ -142,34 +170,50 @@ int	start_ray_casting(t_config *config)
 			if(config->wall.draw_end >= h)
 				config->wall.draw_end = h - 1;
 
-			if (side == 0)
+			if (config->ray.side == 0)
 				wallX = config->player.pos_y + config->ray.wallDist * config->ray.rayDir_y;
 			else
 				wallX = config->player.pos_x + config->ray.wallDist * config->ray.rayDir_x;
 			wallX -= floor((wallX));
 
-			//x coordinate on the texture
-			int texX = int(wallX * double(texWidth));
-			if(side == 0 && rayDirX > 0) texX = texWidth - texX - 1;
-			if(side == 1 && rayDirY < 0) texX = texWidth - texX - 1;
+			config->wall.tex_x = (int)(wallX * (double)h);
+			if (config->ray.side == 0 && config->ray.rayDir_x > 0)
+				config->wall.tex_x = h - config->wall.tex_x - 1; // h=texWidth?
+			if (config->ray.side == 1 && config->ray.rayDir_y < 0)
+				config->wall.tex_x = h - config->wall.tex_x - 1;
 
 			// How much to increase the texture coordinate per screen pixel
-			double step = 1.0 * texHeight / lineHeight;
+			config->wall.step = 1.0 * h / config->wall.line_height;
 			// Starting texture coordinate
-			double texPos = (drawStart - h / 2 + lineHeight / 2) * step;
-			for(int y = drawStart; y<drawEnd; y++)
+			config->wall.tex_pos = (config->wall.draw_start - h / 2 + config->wall.line_height / 2) * config->wall.step;
+			
+			
+			y = config->wall.draw_start;
+			while (y < config->wall.draw_end)
 			{
 				// Cast the texture coordinate to integer, and mask with (texHeight - 1) in case of overflow
-				int texY = (int)texPos & (texHeight - 1);
-				texPos += step;
-				Uint32 color = texture[texNum][texWidth * texY + texX];
+				config->wall.tex_y = (int)config->wall.tex_pos & (h - 1);
+				config->wall.tex_pos += config->wall.step;
+				// t_color c;
+				// c.r =128;
+				// c.g =128;
+				// c.b =128;
+
+				// printf("aaaa\n");
+				mlx_pixel_put(config->data.mlx, config->data.win, y, x, 0x0000FF);
+				
+
 				//make color darker for y-sides: R, G and B byte each divided through two with a "shift" and an "and"
-				if(side == 1) color = (color >> 1) & 8355711;
-				buffer[y][x] = color;
+				// if (config->ray.side == 1)
+				// 	c = (t_color){80, 80, 80};
+				// buffer[y][x] = rgb_to_int(c);
+				
+				// printf("%d\n",rgb_to_int(buffer[y][x]) );
+				y++;
 			}
 
-
 			
+			// draw_buf(&config->data, buffer);
 			x++;
 		}
 	}
